4b0fa17e81f7884b86ed72514fc7d09163984c3f
wugang <wugang@uniontech.com>
2021-05-08 10:56:16 +0800

    feat: porting linuxdmabuf protocol
    From: Roman Gilg <subdiff@gmail.com>
    Date: Wed, 17 Jul 2019 15:48:07 +0200
    Subject: [PATCH] Implement zwp_linux_dmabuf_v1
    
    Summary: This interface provides a way for clients to create generic dmabuf-based wl_buffers.
    
    Test Plan: Works with weston-simple-dmabuf-drm and weston-simple-dmabuf-egl in KWin.
    
    Reviewers: #kwin, #plasma, graesslin, davidedmundson, mart, fredrik
    
    Subscribers: zzag, kde-frameworks-devel, romangg, plasma-devel
    
    Tags: #frameworks, #plasma_on_wayland
    
    Maniphest Tasks: T8067
    
    Differential Revision: https://phabricator.kde.org/D10747
    
    Log: porting linuxdmabuf protocol
    
    Change-Id: I05c943a7ebfbbf4a8f4ae9a95e2e05be9f9aad34

--- src/server/display.cpp
+++ src/server/display.cpp
@@ -61,6 +61,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 #include "strut_interface.h"
 #include "xwayland_keyboard_grab_v1_interface.h"
 #include "screencast_interface.h"
+#include "linuxdmabuf_v1_interface.h"
 
 #include <QCoreApplication>
 #include <QDebug>
@@ -471,6 +472,12 @@ ServerSideDecorationPaletteManagerInterface *Display::createServerSideDecoration
     return b;
 }
 
+LinuxDmabufUnstableV1Interface *Display::createLinuxDmabufInterface(QObject *parent)
+{
+    auto b = new LinuxDmabufUnstableV1Interface(this, parent);
+    connect(this, &Display::aboutToTerminate, b, [this, b] { delete b; });
+    return b;
+}
 
 PlasmaVirtualDesktopManagementInterface *Display::createPlasmaVirtualDesktopManagement(QObject *parent)
 {
